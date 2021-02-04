040d1c1871e4c41592d70466317440f25e01b15d
k1te <kmiller343@gmail.com>
2021-02-03 20:45:31 -0600

    updated to render landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
+import Landing from "./features/landing/Landing";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Router>
+        <Switch>
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,7 +57,7 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      </header> */}
     </div>
   );
 }
