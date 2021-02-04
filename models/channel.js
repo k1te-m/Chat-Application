fd629715c45470dc83c878696f166e4af5bd64fe
k1te <kmiller343@gmail.com>
2021-02-04 16:06:42 -0600

    created channel model

---
+++ models/channel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const channelSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Channel", channelSchema);
