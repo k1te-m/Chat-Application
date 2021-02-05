d0835e45748d50fb6f62329cbe946149b780f935
k1te <kmiller343@gmail.com>
2021-02-04 18:32:18 -0600

    added channel routes to api route index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const channelRoutes = require("./channel");
 
 // API Routes
 router.use("/auth", authRoutes);
+router.use("/channel", channelRoutes);
 
 module.exports = router;
