1421c4e1e60ed62ad15ca5d1d25b6120b99f5253
k1te <kmiller343@gmail.com>
2021-02-09 15:03:39 -0600

    imported SocketContext

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,21 @@ import SignUp from "./features/signup/SignUp";
 import Login from "./features/login/Login";
 import Channel from "./features/channelboard/channels/Channel";
 import Alert from "./features/alert/Alert";
+import SocketContext, { socket } from "./features/context/socket";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Alert />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={Login} />
-          <Route path="/:channel" component={Channel} />
-        </Switch>
+        <SocketContext.Provider value={socket}>
+          <Alert />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
+            <Route path="/:channel" component={Channel} />
+          </Switch>
+        </SocketContext.Provider>
       </Router>
     </div>
   );
