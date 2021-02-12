e950753d3730353985eeef6ca19a09dae979c319
k1te <kmiller343@gmail.com>
2021-02-11 22:12:57 -0600

    updated logo source

--- client/src/features/Logo.js
+++ client/src/features/Logo.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-const LogoImg = styled.img``;
+const LogoImg = styled.img`
+  height: 75px;
+  width: 75px;
+`;
 
 const Logo = () => {
-  return <LogoImg src="../../favicon.ico" />;
+  return <LogoImg src="../../logo192.png" />;
 };
 
 export default Logo;
